96fb1fa10ac30d48c95f378f0269c2be8f1a380c
Shayan Ameen <shayan.ameen.65@gmail.com>
2022-06-04 15:00:27 +0500

    initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import './card.styles.css';
+
+const Card = ({ monster: { email, id, name } }) => (
+  <div className='card-container'>
+    <img
+      alt={`monster ${name}`}
+      src={`https://robohash.org/${id}?set=set2&size=180x180`}
+    />
+    <h2>{name}</h2>
+    <p>{email}</p>
+  </div>
+);
+
+export default Card;
